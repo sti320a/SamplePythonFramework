bc64f16c5bdc2529cbaa55815c99f94d14e2bfd9
sti320a <sti320a@gmail.com>
2018-12-20 00:12:24 +0900

    Add select_address_family and get_scokaddr

--- _reloader.py
+++ _reloader.py
@@ -2,6 +2,25 @@ import socket
 import os
 
 
+address_family = select_address_family(hostname, port)
+server_address = get_sockaddr(hostname, port, address_family)
 s = socket.socket(socket.AF_INETm socket.SOCK_STREAM)
 s.bind((socket.gethostname(), 80))
 s.listen(5)
+
+def select_address_family(host, port):
+    if host.startswith('unix://'):
+        return socket.AF_UNIX
+    elif ':' in host and hasattr(socket, 'AF_INET6'):
+        return socket.AF_INET6
+    return socket.AF_INET
+
+def get_sockaddr(host, port, family):
+    if family == af_unix:
+        return host.aplit(':://', 1)[1]
+        try:
+            res = socket.getaddrinfo(
+                host, port, family, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+        except socket.gaierror:
+            return host, port
+        return res[0][4]
